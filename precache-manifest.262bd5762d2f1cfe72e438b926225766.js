6f280aa27854bb9bfd441c77589ab7e36e9411c1
bubben <claesson1998@gmail.com>
2019-03-14 22:58:33 +0100

    Updates

---
+++ precache-manifest.262bd5762d2f1cfe72e438b926225766.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c90b415f342b24257720",
+    "url": "/PORTFOLIO/static/js/runtime~main.bba35dba.js"
+  },
+  {
+    "revision": "7b2646713fb19fb14cad",
+    "url": "/PORTFOLIO/static/js/main.0cc47bc1.chunk.js"
+  },
+  {
+    "revision": "71aee167f733c84e2cce",
+    "url": "/PORTFOLIO/static/js/2.e6f38151.chunk.js"
+  },
+  {
+    "revision": "7b2646713fb19fb14cad",
+    "url": "/PORTFOLIO/static/css/main.27a3efed.chunk.css"
+  },
+  {
+    "revision": "cf162d44d8553d905bf045e84047a193",
+    "url": "/PORTFOLIO/index.html"
+  }
+];
